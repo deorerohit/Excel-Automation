1b5904124066d763ab561e14ce9891811746b16c
dhairy913 <dhairyapotbhare913@gmail.com>
2020-10-22 12:58:25 +0530

    Concatinate Given no of File into output File

---
+++ Addition_Excel_File.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Enter the no of Excel no
+print('Enter no of file : ')
+i = int(input())
+
+# store the File Descriptor in list
+lst = list()
+for fd in range(0,i) :
+    temp = input('Enter the File Path')
+    k = pd.read_excel(str(temp))
+    lst.append(k)
+    
+#Concate the file
+dp = pd.concat(lst,axis=1)
+
+#Export Dataframe to Excel file
+dp.to_excel('D:\Java Program\Data.xlsx',index=True)
