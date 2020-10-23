91732a60c719ff2346970d5970d1092ff0915e4f
dhairy913 <dhairyapotbhare913@gmail.com>
2020-10-23 09:51:07 +0530

    Update names

--- Finding_common_column.py
+++ Finding_common_column.py
@@ -4,26 +4,27 @@ print('Enter no of file : ')
 i = int(input())
 
 # Creating list of col_names in given xlsx files
-lst = list()
+lst_column_names = list()
 filesD = []
 
 for fd in range(0, i):
     temp = input('Enter the File Path')
     k = pd.read_excel(str(temp))
     filesD.append(k)
-    lst.append(k.columns.ravel())
+    lst_column_names.append(k.columns.ravel())
 
 # finding the common column
-com = list(set.intersection(*map(set, lst)))
+com_column = list(set.intersection(*map(set, lst_column_names)))
 
-p = []
+df_com_column = []
 # Reading common column from each .xlsx file
 for i in filesD:
-    for j in com:
-        df = pd.DataFrame(i[str(j)])
-        print(df)
-        p.append(df)
+    for j in com_column:
+        temp_df = pd.DataFrame(i[str(j)])
+        print(temp_df)
+        df_com_column.append(temp_df)
 
-#Writing Common column of all files in new created .xlsx File column
-dp = pd.concat(p, axis=1)
+# Writing Common column of all files in new created .xlsx File column
+dp = pd.concat(df_com_column, axis=1)
 dp.to_excel('D:\\text.xlsx', index=False)
+
